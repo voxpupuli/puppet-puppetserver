387a49320958b9ef24c2265ddb353956028b6242
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-10-28 16:02:40 +0100

    Release 0.3.0

--- spec/acceptance/puppetserver_spec.rb
+++ spec/acceptance/puppetserver_spec.rb
@@ -9,6 +9,7 @@ describe 'puppetserver' do
       EOS
 
       apply_manifest(pp, :catch_failures => true)
+      sleep 60
       apply_manifest(pp, :catch_changes => true)
     end
 
@@ -22,10 +23,42 @@ describe 'puppetserver' do
     end
 
     describe port(8140) do
-      it 'should listen on port 8140' do
-        sleep 60
-        should be_listening
-      end
+      it { should be_listening }
+    end
+  end
+
+  context 'when overriding ssl-port' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'puppetserver':
+          config => {
+            'webserver' => {
+              'ssl-port' => '18140',
+            },
+          },
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      sleep 60
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe package('puppetserver') do
+      it { should be_installed }
+    end
+
+    describe service('puppetserver') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe port(18140) do
+      it { should be_listening }
+    end
+
+    describe port(8140) do
+      it { should_not be_listening }
     end
   end
 end
