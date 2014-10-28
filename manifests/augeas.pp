190a7f210791765a35ad2d19e18a7aae26fd0c49
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-28 13:34:52 +0100

    Move test_trapperkeeper.aug

--- manifests/augeas.pp
+++ manifests/augeas.pp
@@ -3,6 +3,6 @@ class puppetserver::augeas {
   ::augeas::lens { 'trapperkeeper':
     ensure      => 'present',
     lens_source => "puppet:///modules/${module_name}/lenses/trapperkeeper.aug",
-    test_source => "puppet:///modules/${module_name}/lenses/tests/test_trapperkeeper.aug",
+    test_source => "puppet:///modules/${module_name}/lenses/test_trapperkeeper.aug",
   }
 }
