5e34a2c496c7bfc8c7d1f2960a4f4a3349808f12
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-28 11:03:10 +0100

    Add puppetserver::config::puppetserver and set webserver/port

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,26 +1,32 @@
 class puppetserver::config {
   if has_key($::puppetserver::config, 'java_args') {
     if has_key($::puppetserver::config['java_args'], 'xms') {
-      ::puppetserver::config::java_arg {
-        '-Xms':
-          ensure => 'present',
-          value  => $::puppetserver::config['java_args']['xms'],
+      ::puppetserver::config::java_arg { '-Xms':
+        ensure => 'present',
+        value  => $::puppetserver::config['java_args']['xms'],
       }
     }
 
     if has_key($::puppetserver::config['java_args'], 'xmx') {
-      ::puppetserver::config::java_arg {
-        '-Xmx':
-          ensure => 'present',
-          value  => $::puppetserver::config['java_args']['xmx'],
+      ::puppetserver::config::java_arg { '-Xmx':
+        ensure => 'present',
+        value  => $::puppetserver::config['java_args']['xmx'],
       }
     }
 
     if has_key($::puppetserver::config['java_args'], 'maxpermsize') {
-      ::puppetserver::config::java_arg {
-        '-XX:MaxPermSize=':
-          ensure => 'present',
-          value  => $::puppetserver::config['java_args']['maxpermsize'],
+      ::puppetserver::config::java_arg { '-XX:MaxPermSize=':
+        ensure => 'present',
+        value  => $::puppetserver::config['java_args']['maxpermsize'],
+      }
+    }
+  }
+
+  if has_key($puppetserver::config, 'webserver') {
+    if has_key($puppetserver::config['webserver'], 'port') {
+      ::puppetserver::config::puppetserver { 'webserver.conf/port':
+        ensure => 'present',
+        value  => $puppetserver::config['webserver']['port'],
       }
     }
   }
---
+++ manifests/config/puppetserver.pp
@@ -0,0 +1,31 @@
+define puppetserver::config::puppetserver (
+  $value,
+  $ensure = 'present',
+) {
+  $targetdir = '/etc/puppetserver/conf.d'
+  $target = "${targetdir}${file}"
+
+  case $ensure {
+    'present': {
+      $changes = [
+        "set /files${targetdir}/${name} '${value}'",
+      ]
+    }
+
+    'absent': {
+      $changes = [
+        "rm /files${targetdir}/${name}",
+      ]
+    }
+
+    default: {
+      fail 'Wrong value for "ensure".'
+    }
+  }
+
+  augeas { "Set puppetserver config ${title}":
+    lens    => 'Trapperkeeper.lns',
+    incl    => "${targetdir}/*",
+    changes => $changes,
+  }
+}
