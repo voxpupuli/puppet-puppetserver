0583d32a01be5285c6cf2d3a73f810e63b9b3d7b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-28 11:41:21 +0100

    Deploy Augeas trapperkeeper lens

---
+++ manifests/augeas.pp
@@ -0,0 +1,7 @@
+class puppetserver::augeas {
+  ::augeas::lens { 'trapperkeeper':
+    ensure      => 'present',
+    lens_source => "puppet:///modules/${module_name}/lenses/trapperkeeper.aug",
+    test_source => "puppet:///modules/${module_name}/lenses/tests/test_trapperkeeper.aug",
+  }
+}
