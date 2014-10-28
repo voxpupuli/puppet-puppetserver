e6087af6379c4d4f3e0e81d329278cf61b35e5b0
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-10-28 12:10:37 +0100

    Require the augeas class (augeas-tools needed)

--- manifests/augeas.pp
+++ manifests/augeas.pp
@@ -1,5 +1,5 @@
 class puppetserver::augeas {
-  include ::augeas
+  require ::augeas
   ::augeas::lens { 'trapperkeeper':
     ensure      => 'present',
     lens_source => "puppet:///modules/${module_name}/lenses/trapperkeeper.aug",
