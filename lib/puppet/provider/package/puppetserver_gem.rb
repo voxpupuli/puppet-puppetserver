bf6e88d3fdc55e6c9bbabefe6f4e34d73832a424
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-02-16 14:18:44 +0100

    Add puppetserver_gem provider for the package type

---
+++ lib/puppet/provider/package/puppetserver_gem.rb
@@ -0,0 +1,14 @@
+Puppet::Type.type(:package).provide :puppetserver_gem, :parent => :gem do
+  desc "Puppet Server Ruby Gem support. If a URL is passed via `source`, then
+    that URL is used as the remote gem repository; if a source is present but is
+    not a valid URL, it will be interpreted as the path to a local gem file.  If
+    source is not present at all, the gem will be installed from the default gem
+    repositories."
+ 
+  has_feature :versionable
+ 
+  def self.command(name)
+    return [which('puppetserver'), 'gem'] if name == :gemcmd
+    super
+  end
+end
