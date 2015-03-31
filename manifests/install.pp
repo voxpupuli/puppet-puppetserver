a9d019ad9983be057726e1f8c7d4ae4426078758
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-31 16:05:18 +0200

    Revert "Fix /var/lib/puppet/ssl ownership on Debian7"
    This reverts commit 4f7885bfcfb8dd3057d51e7cc537cb069d69624d.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,5 @@
 class puppetserver::install {
   package { 'puppetserver':
     ensure => $::puppetserver::version,
-  } ->
-  # puppetserver 1.0.2 Debian package does not properly set ownership
-  file { '/var/lib/puppet/ssl':
-    ensure  => directory,
-    owner   => 'puppet',
-    group   => 'puppet',
-    recurse => true,
   }
 }
