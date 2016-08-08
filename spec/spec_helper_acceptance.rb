a3fdbb9bc7052787b7cc84613d1473cf211a4634
Tim Meusel <tim@bastelfreak.de>
2016-08-08 11:09:57 +0200

    rubocop: fix Style/SpaceAfterComma

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -22,10 +22,10 @@ RSpec.configure do |c|
     puppet_module_install(source: proj_root, module_name: 'puppetserver')
 
     hosts.each do |host|
-      on host, puppet('module','install','puppetlabs-apt'), acceptable_exit_codes: [0,1]
-      on host, puppet('module','install','puppetlabs-stdlib'), acceptable_exit_codes: [0,1]
-      on host, puppet('module','install','herculesteam-augeasproviders_core'), acceptable_exit_codes: [0,1]
-      on host, puppet('module','install','camptocamp-augeas'), acceptable_exit_codes: [0,1]
+      on host, puppet('module', 'install', 'puppetlabs-apt'), acceptable_exit_codes: [0, 1]
+      on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
+      on host, puppet('module', 'install', 'herculesteam-augeasproviders_core'), acceptable_exit_codes: [0, 1]
+      on host, puppet('module', 'install', 'camptocamp-augeas'), acceptable_exit_codes: [0, 1]
     end
 
     pp = <<-EOS
