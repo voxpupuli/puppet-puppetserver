92a819c0364d8c34c5999b57527b2bd28cb84c07
Tim Meusel <tim@bastelfreak.de>
2016-09-01 14:58:48 +0200

    Revert "Revert "modulesync 0.12.5""

--- spec/unit/puppet/provider/puppetserver_config/augeas_spec.rb
+++ spec/unit/puppet/provider/puppetserver_config/augeas_spec.rb
@@ -5,126 +5,126 @@ require 'spec_helper'
 provider_class = Puppet::Type.type(:puppetserver_config).provider(:augeas)
 
 describe provider_class do
-  context "with empty file" do
-    let(:tmptarget) { aug_fixture("empty") }
+  context 'with empty file' do
+    let(:tmptarget) { aug_fixture('empty') }
     let(:target) { tmptarget.path }
 
-    it "should create simple new entry" do
+    it 'creates simple new entry' do
       apply!(Puppet::Type.type(:puppetserver_config).new(
-        :name     => "foo",
-        :key      => "foo",
-        :path     => "bar",
-        :value    => "42",
-        :target   => target,
-        :provider => "augeas"
+               name: 'foo',
+               key: 'foo',
+               path: 'bar',
+               value: '42',
+               target: target,
+               provider: 'augeas'
       ))
 
-      aug_open(target, "Trapperkeeper.lns") do |aug|
-        expect(aug.get("@hash/@simple/@value")).to eq("42")
+      aug_open(target, 'Trapperkeeper.lns') do |aug|
+        expect(aug.get('@hash/@simple/@value')).to eq('42')
       end
     end
 
-    it "should create an array entry" do
+    it 'creates an array entry' do
       apply!(Puppet::Type.type(:puppetserver_config).new(
-        :name     => "foo",
-        :key      => "foo",
-        :path     => "bar",
-        :value    => ["42", "24"],
-        :target   => target,
-        :provider => "augeas"
+               name: 'foo',
+               key: 'foo',
+               path: 'bar',
+               value: %w(42 24),
+               target: target,
+               provider: 'augeas'
       ))
 
-      aug_open(target, "Trapperkeeper.lns") do |aug|
-        expect(aug.get("@hash/@array/1")).to eq("42")
-        expect(aug.get("@hash/@array/2")).to eq("24")
+      aug_open(target, 'Trapperkeeper.lns') do |aug|
+        expect(aug.get('@hash/@array/1')).to eq('42')
+        expect(aug.get('@hash/@array/2')).to eq('24')
       end
     end
   end
 
-  context "with full file" do
-    let(:tmptarget) { aug_fixture("full") }
+  context 'with full file' do
+    let(:tmptarget) { aug_fixture('full') }
     let(:target) { tmptarget.path }
 
-    describe "when creating settings" do
-      it "should create a simple entry" do
+    describe 'when creating settings' do
+      it 'creates a simple entry' do
         apply!(Puppet::Type.type(:puppetserver_config).new(
-          :name     => "foo",
-          :key      => "foo",
-          :path     => "bar",
-          :value    => "42",
-          :target   => target,
-          :provider => "augeas"
+                 name: 'foo',
+                 key: 'foo',
+                 path: 'bar',
+                 value: '42',
+                 target: target,
+                 provider: 'augeas'
         ))
 
-        aug_open(target, "Trapperkeeper.lns") do |aug|
-          expect(aug.get("@hash/@simple[.='foo']/@value")).to eq("42")
+        aug_open(target, 'Trapperkeeper.lns') do |aug|
+          expect(aug.get("@hash/@simple[.='foo']/@value")).to eq('42')
         end
       end
 
-      it "should create an array entry" do
+      it 'creates an array entry' do
         apply!(Puppet::Type.type(:puppetserver_config).new(
-          :name     => "foo",
-          :key      => "foo",
-          :path     => "bar",
-          :value    => ["42", "24"],
-          :target   => target,
-          :provider => "augeas"
+                 name: 'foo',
+                 key: 'foo',
+                 path: 'bar',
+                 value: %w(42 24),
+                 target: target,
+                 provider: 'augeas'
         ))
 
-        aug_open(target, "Trapperkeeper.lns") do |aug|
-          expect(aug.get("@hash/@array[.='foo']/1")).to eq("42")
-          expect(aug.get("@hash/@array[.='foo']/2")).to eq("24")
+        aug_open(target, 'Trapperkeeper.lns') do |aug|
+          expect(aug.get("@hash/@array[.='foo']/1")).to eq('42')
+          expect(aug.get("@hash/@array[.='foo']/2")).to eq('24')
         end
       end
     end
 
-    describe "when deleting settings" do
-      it "should delete a setting" do
+    describe 'when deleting settings' do
+      it 'deletes a setting' do
         apply!(Puppet::Type.type(:puppetserver_config).new(
-          :name     => "client-whitelist",
-          :key      => "client-whitelist",
-          :path     => "puppet-admin",
-          :ensure   => :absent,
-          :type     => :array,
-          :target   => target,
-          :provider => "augeas"
+                 name: 'client-whitelist',
+                 key: 'client-whitelist',
+                 path: 'puppet-admin',
+                 ensure: :absent,
+                 type: :array,
+                 target: target,
+                 provider: 'augeas'
         ))
 
-        aug_open(target, "Trapperkeeper.lns") do |aug|
+        aug_open(target, 'Trapperkeeper.lns') do |aug|
           expect(aug.match("@hash/@array[.='client-whitelist']").size).to eq(0)
         end
       end
     end
 
-    describe "when updating settings" do
-      it "should replace a setting" do
+    describe 'when updating settings' do
+      it 'replaces a setting' do
         apply!(Puppet::Type.type(:puppetserver_config).new(
-          :name     => "client-whitelist",
-          :key      => "client-whitelist",
-          :path     => "puppet-admin",
-          :value    => [ "foo", "bar" ],
-          :target   => target,
-          :provider => "augeas"
+                 name: 'client-whitelist',
+                 key: 'client-whitelist',
+                 path: 'puppet-admin',
+                 value: %w(foo bar),
+                 target: target,
+                 provider: 'augeas'
         ))
 
-        aug_open(target, "Trapperkeeper.lns") do |aug|
-          expect(aug.get("@hash/@array[.='client-whitelist']/1")).to eq("foo")
-          expect(aug.get("@hash/@array[.='client-whitelist']/2")).to eq("bar")
+        aug_open(target, 'Trapperkeeper.lns') do |aug|
+          expect(aug.get("@hash/@array[.='client-whitelist']/1")).to eq('foo')
+          expect(aug.get("@hash/@array[.='client-whitelist']/2")).to eq('bar')
         end
       end
     end
   end
 
-  context "with broken file" do
-    let(:tmptarget) { aug_fixture("broken") }
+  context 'with broken file' do
+    let(:tmptarget) { aug_fixture('broken') }
     let(:target) { tmptarget.path }
 
-    it "should fail to load" do
+    it 'fails to load' do
       txn = apply(Puppet::Type.type(:puppetserver_config).new(
-        :name     => "foo",
-        :value    => "yes",
-        :target   => target,
-        :provider => "augeas"
+                    name: 'foo',
+                    value: 'yes',
+                    target: target,
+                    provider: 'augeas'
       ))
 
       expect(txn.any_failed?).not_to eq(nil)
@@ -133,4 +133,3 @@ describe provider_class do
     end
   end
 end
-
