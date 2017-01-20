ce937ee0d8520794c7a17cf4ea3efe06d724f65e
Thomas Mueller <thomas@chaschperli.ch>
2017-01-20 21:32:05 +0100

    Replace spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,13 +1,8 @@
-require 'beaker-rspec'
+require 'beaker-rspec/spec_helper'
+require 'beaker-rspec/helpers/serverspec'
+require 'beaker/puppet_install_helper'
 
-hosts.each do |host|
-  # Install Puppet
-  if ENV['PUPPET_AIO']
-    install_puppet_agent_on host, {}
-  else
-    install_puppet_on host
-  end
-end
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
 
 RSpec.configure do |c|
   # Project root
@@ -20,18 +15,21 @@ RSpec.configure do |c|
   c.before :suite do
     # Install module and dependencies
     puppet_module_install(source: proj_root, module_name: 'puppetserver')
-
     hosts.each do |host|
       on host, puppet('module', 'install', 'puppetlabs-apt'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'herculesteam-augeasproviders_core'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'camptocamp-augeas'), acceptable_exit_codes: [0, 1]
     end
+  end
+end
 
-    pp = <<-EOS
-      class { 'puppetserver::repository': }
-    EOS
+shared_examples 'a idempotent resource' do
+  it 'applies with no errors' do
+    apply_manifest(pp, catch_failures: true)
+  end
 
-    apply_manifest_on(hosts, pp, catch_failures: false)
+  it 'applies a second time without changes' do
+    apply_manifest(pp, catch_changes: true)
   end
 end
